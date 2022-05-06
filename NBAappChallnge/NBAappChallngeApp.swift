433466354f5967d7abda18fcb978c995849f6ddb
idan cohen <idan000189@gmail.com>
2022-05-06 13:47:20 +0300

    Initial Commit

---
+++ NBAappChallnge/NBAappChallngeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBAappChallngeApp.swift
+//  NBAappChallnge
+//
+//  Created by idan Cohen on 06/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct NBAappChallngeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
